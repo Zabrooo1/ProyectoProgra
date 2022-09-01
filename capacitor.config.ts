f63dd0fd4381f8a21c412b4543e5d22174453ade
Zabrooo1 <101838453+Zabrooo1@users.noreply.github.com>
2022-09-01 14:41:01 -0400

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Foik',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
